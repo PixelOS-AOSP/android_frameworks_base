cfbaf420f487bae36ec89be1547c308d3b44fc42
Pranav Vashi <neobuddy89@gmail.com>
2022-07-02 02:48:55 +0530

    SystemUI: Add current divider config for lockscreen charging
    * Some modern devices measure directly in mA instead uA.
    * We will let devices specify config_currentInfoDivider to check
      if uA to mA conversion is required.
    * Use same divider for wattage calculation.
    
    Change-Id: I14be75cb2871379e95edf4c4b2461b693f5d859b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    (cherry picked from commit 0b46b4ce5060ec0c79351d8ad28d0c174af3778d)

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -242,6 +242,8 @@ public class KeyguardIndicationController {
     private boolean mInited;
     private boolean mFaceDetectionRunning;
 
+    private int mCurrentDivider;
+
     private KeyguardUpdateMonitorCallback mUpdateMonitorCallback;
 
     private boolean mDozing;
@@ -425,6 +427,8 @@ public class KeyguardIndicationController {
         mKeyguardStateController.addCallback(mKeyguardStateCallback);
 
         mStatusBarStateListener.onDozingChanged(mStatusBarStateController.isDozing());
+
+        mCurrentDivider = mContext.getResources().getInteger(R.integer.config_currentInfoDivider);
     }
 
     @Nullable
@@ -672,7 +676,7 @@ public class KeyguardIndicationController {
         if (mBatteryPresent && (mPowerPluggedIn || mEnableBatteryDefender)) {
             String powerIndication = computePowerIndication();
             if (DEBUG_CHARGING_SPEED) {
-                powerIndication += ",  " + (mChargingWattage / 1000) + " mW";
+                powerIndication += ",  " + (mChargingWattage / mCurrentDivider) + " mW";
             }
 
             mKeyguardLogger.logUpdateBatteryIndication(powerIndication, mPowerPluggedIn);
