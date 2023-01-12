edf964f0ba71c47e6a77e7e996ba6bdecead89f5
ShevT <evgeny.shishkov@gmail.com>
2023-01-12 14:24:12 +0300

    SystemUI: Add config overlay for battery info update
     It is not necessary to use this for all devices, but only for those
     that have a problem with charging display (for example OnePlus 7 series)
    
    Change-Id: I98441087ec450ec3a2699a1a79b84bc32a1fe340
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    (cherry picked from commit 614a7514e8792270205122459b0204c4bf89a4f8)

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -248,6 +248,7 @@ public class KeyguardIndicationController {
     private int mCurrentDivider;
 
     private IBatteryPropertiesRegistrar mBatteryPropertiesRegistrar;
+    private boolean mAlternateFastchargeInfoUpdate;
 
     private KeyguardUpdateMonitorCallback mUpdateMonitorCallback;
 
@@ -435,9 +436,13 @@ public class KeyguardIndicationController {
 
         mCurrentDivider = mContext.getResources().getInteger(R.integer.config_currentInfoDivider);
 
-        mBatteryPropertiesRegistrar =
+        mAlternateFastchargeInfoUpdate =
+                    mContext.getResources().getBoolean(R.bool.config_alternateFastchargeInfoUpdate);
+        if (mAlternateFastchargeInfoUpdate) {
+            mBatteryPropertiesRegistrar =
                     IBatteryPropertiesRegistrar.Stub.asInterface(
                     ServiceManager.getService("batteryproperties"));
+        }
     }
 
     @Nullable
@@ -1492,7 +1497,7 @@ public class KeyguardIndicationController {
                 mKeyguardLogger.log(TAG, ERROR, "Error calling IBatteryStats", e);
                 mChargingTimeRemaining = -1;
             }
-            if (wasPluggedIn != mPowerPluggedIn) {
+            if (mAlternateFastchargeInfoUpdate && (wasPluggedIn != mPowerPluggedIn)) {
                 if (mPowerPluggedIn) {
                     mUpdateInfo.run();
                 } else {
