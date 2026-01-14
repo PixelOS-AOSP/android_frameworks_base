8cab85dde9f0ffe6b8156a719f74c19a3900fa0c
Dmitrii <bankersenator@gmail.com>
2026-01-14 04:49:52 +0000

    VolumeDialog: Fix layout issues with left gravity
    * Prevent clipping of landscape ringer dialog
    
    Change-Id: I0761ea04e46312377c504f10b345a59d68df5d92
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/dialog/VolumeDialog.kt
+++ packages/SystemUI/src/com/android/systemui/volume/dialog/VolumeDialog.kt
@@ -78,7 +78,14 @@ constructor(
             )
         val volumePanelOnLeft =
             (configuredGravity and Gravity.HORIZONTAL_GRAVITY_MASK) == Gravity.LEFT
-        val side = if (volumePanelOnLeft) Gravity.START else Gravity.END
+        val side = if (volumePanelOnLeft) Gravity.LEFT else Gravity.RIGHT
+        val dialogView = win.decorView
+
+        dialogView.layoutDirection =
+            if (volumePanelOnLeft)
+                View.LAYOUT_DIRECTION_RTL
+            else
+                View.LAYOUT_DIRECTION_LTR
 
         if (isVolumeDialogVertical) {
             win.setLayout(
