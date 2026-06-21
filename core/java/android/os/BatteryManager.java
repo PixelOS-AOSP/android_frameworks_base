07bf22d451dee816fc885b4c4c45b008f3f4f945
Michael Bestas <mkbestas@lineageos.org>
2026-06-21 04:56:22 +0300

    fixup! BatteryService: Expose [maximum|design] capacity API
    Change-Id: I894e127b463efe7535d7ebe39e0e80ba8ad91e5b

--- core/java/android/os/BatteryManager.java
+++ core/java/android/os/BatteryManager.java
@@ -176,14 +176,14 @@ public class BatteryManager {
     /**
      * Extra for {@link android.content.Intent#ACTION_BATTERY_CHANGED}:
      * Int value representing the estimated battery full charge capacity in microampere-hours.
-     * {@hide}
+     * @hide
      */
     public static final String EXTRA_MAXIMUM_CAPACITY = "android.os.extra.MAXIMUM_CAPACITY";
 
     /**
      * Extra for {@link android.content.Intent#ACTION_BATTERY_CHANGED}:
      * Int value representing the battery full charge design capacity in microampere-hours.
-     * {@hide}
+     * @hide
      */
     public static final String EXTRA_DESIGN_CAPACITY = "android.os.extra.DESIGN_CAPACITY";
 
