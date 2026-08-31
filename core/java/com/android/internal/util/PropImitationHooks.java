c52e06c5fcce08c6d48561bb9b0be2af44400ab9
hridaya <hridaya@pixelos.net>
2026-08-31 23:06:04 +0545

    unsafe
    Change-Id: I86aa4c5b73729c4f005d113ad93b4dde052fe621

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -52,6 +52,8 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 
+import sun.misc.Unsafe;
+
 /**
  * @hide
  */
@@ -121,6 +123,29 @@ public class PropImitationHooks {
     private static volatile String sProcessName;
     private static volatile boolean sIsGms, sIsFinsky, sIsPhotos;
 
+    private static final Field OFFSET_FIELD;
+    private static final Unsafe UNSAFE;
+
+    static {
+        Unsafe unsafe = null;
+        Field offsetField = null;
+
+        try {
+            Field field = Unsafe.class.getDeclaredField("theUnsafe");
+            field.setAccessible(true);
+            unsafe = (Unsafe) field.get(null);
+            field.setAccessible(false);
+
+            offsetField = Field.class.getDeclaredField("offset");
+            offsetField.setAccessible(true);
+        } catch (Exception e) {
+            Log.e(TAG, "Unable to initialize Unsafe", e);
+        }
+
+        UNSAFE = unsafe;
+        OFFSET_FIELD = offsetField;
+    }
+
     public static void setProps(Context context) {
         final String packageName = context.getPackageName();
         final String processName = Application.getProcessName();
@@ -168,6 +193,13 @@ public class PropImitationHooks {
     }
 
     private static void setPropValue(String key, String value) {
+        if (UNSAFE == null || OFFSET_FIELD == null) {
+            Log.e(TAG, "Unsafe is unavailable", new IllegalStateException());
+            return;
+        }
+
+        Field field = null;
+
         try {
             dlog("Setting prop " + key + " to " + value.toString());
             Class clazz = Build.class;
@@ -175,13 +207,21 @@ public class PropImitationHooks {
                 clazz = Build.VERSION.class;
                 key = key.substring(8);
             }
-            Field field = clazz.getDeclaredField(key);
+            field = clazz.getDeclaredField(key);
             field.setAccessible(true);
             // Cast the value to int if it's an integer field, otherwise string.
-            field.set(null, field.getType().equals(Integer.TYPE) ? Integer.parseInt(value) : value);
-            field.setAccessible(false);
+            long offset = OFFSET_FIELD.getInt(field);
+            if (field.getType().equals(Integer.TYPE)) UNSAFE.putInt(field.getDeclaringClass(), offset, Integer.parseInt(value));
+            else UNSAFE.putObject(field.getDeclaringClass(), offset, value);
+
         } catch (Exception e) {
             Log.e(TAG, "Failed to set prop " + key, e);
+        } finally {
+            if (field != null) {
+                try {
+                    field.setAccessible(false);
+                } catch (Exception ignored) {}
+            }
         }
     }
 
